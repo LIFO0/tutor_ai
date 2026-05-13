31641a077cc9d3574a4c14a3408f05dba0617daf
LIFO0 <lera.ryzhenkova@inbox.ru>
2026-05-13 12:09:08 +0300

    Refactor global CSS to enforce a light theme across the application. Remove dark mode styles and update layout components to reflect the new theming approach.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-full w-full bg-zinc-200 dark:bg-black">{children}</div>
+    <div className="min-h-full w-full bg-zinc-200">{children}</div>
   );
 }
 
